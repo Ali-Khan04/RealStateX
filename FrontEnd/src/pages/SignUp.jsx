499f84e0054d1a1bc751d9e14dcb2f11a44d4e62
Ali-Khan04 <aliiiiahmaaeed@gmail.com>
2025-08-12 10:57:24 +0500

    add user SignUp form

--- FrontEnd/src/pages/SignUp.jsx
+++ FrontEnd/src/pages/SignUp.jsx
@@ -1,5 +1,37 @@
 import React from "react";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="max-w-md mx-auto mt-12 p-6 bg-white rounded-lg shadow-md">
+      <h1 className="text-4xl text-center font-extrabold mb-6 text-gray-800">
+        SignUp
+      </h1>
+      <form className="flex flex-col gap-5">
+        <input
+          className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+          type="text"
+          required
+          placeholder="UserName"
+        />
+        <input
+          className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+          type="email"
+          required
+          placeholder="Email"
+        />
+        <input
+          className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+          type="password"
+          required
+          placeholder="Password"
+        />
+        <button
+          type="submit"
+          className="bg-blue-600 text-white font-semibold py-3 rounded-lg hover:bg-blue-700 transition"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 }
